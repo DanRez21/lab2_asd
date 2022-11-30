3528c6717b02ff1a4182fbfa7cd10adc09924594
DanRez21 <danielreznik3@gmail.com>
2022-11-30 13:20:42 +0200

    first commit

---
+++ danya_lab2.py
@@ -0,0 +1,15 @@
+from PIL import Image, ImageDraw
+
+with open('DS0.txt') as f:
+
+    img = Image.new('RGB', (960, 540), '#ffffff')
+    draw = ImageDraw.Draw(img)
+
+    for i in f.readlines():
+
+        coord = i.split(' ')
+        x, y = float(coord[0]), float(coord[1])
+        draw.point((x, y), '#0000ff')
+
+    img.save('image2.png')
+    
